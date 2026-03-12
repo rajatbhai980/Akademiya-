10216c5076fd093c09ac446024edc37e4c18c008
rajatbhai980 <raiz41455@gmail.com>
2026-03-12 19:13:03 +0545

    created base models

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ I am personally going to use this site.</p>
 <ol>
 <img src='docs/initial_requirements.png'>
 <img src='docs/initial_schema.png'>
+
+--> Create .env.local and add the things listed below. we are using postgresql for database. https://medium.com/django-unleashed/complete-tutorial-set-up-postgresql-database-with-django-application-d9e789ffa384
+```
+DEBUG=True
+ALLOWED_HOSTS=localhost,127.0.0.1
+
+DB_NAME
+DB_USER
+DB_PASSWORD 
+DB_HOST 
+DB_PORT
+```
--- base/models.py
+++ base/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField()
+    semester = models.CharField(null=True, blank=True, max_length=20)
+    bio = models.CharField(null=True, blank=True, max_length=100)
+    subscribed = models.BooleanField(default=False)
+    follows = models.ManyToManyField(User, related_name='follower')
+    gems = models.IntegerField(default=0)
+
+
+class Performance(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='performance')
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+    solved = models.IntegerField(default=0)
+    correct = models.IntegerField(default=0)
+    wrong = models.IntegerField(default=0)
+    correct_ratio = models.DecimalField(default=0, decimal_places=3, max_digits=3)
+
+class Semester(models.Model): 
+    name = models.CharField(max_length=40)
+
+class Subject(models.Model):
+    name= models.CharField(max_length=40)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name='subjects')
+
+class Question(models.Model): 
+    description = models.CharField(max_length=30)
+    subject = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name='questions')
+    hint = models.CharField(max_length=40)
+    full_explaination = models.CharField(max_length=100)
+
+class Answer(models.Model): 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    description = models.CharField(max_length=40)
+    coorect = models.BooleanField()
+    
+
+
