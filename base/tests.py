4bbcbaf6da5bbc70c9005b9c66b08e92b37fccbe
rajatbhai980 <raiz41455@gmail.com>
2026-08-09 20:52:24 +0545

    removed email test

--- base/tests.py
+++ base/tests.py
@@ -4,18 +4,3 @@ import os
 from django.test import SimpleTestCase
 
 
-class SettingsDefaultsTests(SimpleTestCase):
-    def test_missing_email_port_defaults_to_587(self):
-        settings_module = importlib.import_module('Akademiya.settings')
-        original_email_port = os.environ.get('EMAIL_PORT')
-        os.environ.pop('EMAIL_PORT', None)
-
-        try:
-            reloaded_settings = importlib.reload(settings_module)
-            self.assertEqual(reloaded_settings.EMAIL_PORT, 587)
-        finally:
-            if original_email_port is None:
-                os.environ.pop('EMAIL_PORT', None)
-            else:
-                os.environ['EMAIL_PORT'] = original_email_port
-            importlib.reload(settings_module)
