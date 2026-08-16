7dc49243208b97ac5fc167ac799c7a308ab1add1
rajatbhai980 <raiz41455@gmail.com>
2026-08-16 11:42:38 +0545

    additional fields for url redirect and displaying image

--- leaderboard/serializers.py
+++ leaderboard/serializers.py
@@ -4,7 +4,8 @@ from base.models import Scholar
 
 class ScholarPerformanceSerializer(serializers.ModelSerializer):
     level = serializers.IntegerField(source='performance.level')
+    photo = serializers.ImageField(use_url=True, allow_null=True, required=False)
 
     class Meta:
         model = Scholar
-        fields = ['username', 'level']
+        fields = ['id', 'photo', 'username', 'level']
